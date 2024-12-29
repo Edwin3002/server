8bdf7bd4d5f2cfdbd7e652c4a58572717d948ae3
Edwin Vargas Ayala <88119497+Edwin3002@users.noreply.github.com>
2024-12-29 14:36:59 -0500

    Add files via upload

---
+++ sockets/socket.js
@@ -0,0 +1,27 @@
+const { io } = require('../index');
+
+
+// Mensajes de Sockets
+io.on('connection', client => {
+    console.log('Cliente conectado');
+
+    client.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+
+    client.on('mensaje', ( payload ) => {
+        console.log('Mensaje', payload);
+
+        io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
+
+    });
+
+    client.on('send-msg', ( payload ) => {
+console.log(payload);
+
+        io.emit( 'newMsg', { msg: "new"} );
+
+    });
+
+
+});
